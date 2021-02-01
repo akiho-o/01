b3d174fb2ccc00ac1cc523ecfc7786c8168f34d3
akiho-o <akiho.o.0930@gmail.com>
2021-02-01 19:44:12 +0900

    コード修正

--- array_3.php
+++ array_3.php
@@ -4,17 +4,11 @@ $nums = [[1,2], [3, 4], [5,6,7]];
 
 // 以下に出力するコードを記述
 
-$nams = [
-    '1' => ['1','2'],
-    '2' => ['3', '4'],
-    '3' => ['5', '6', '7']
-];
-
-echo $nams['2'][1] . '<br>';
-echo $nams['3'][2] . '<br>';
-echo $nams['3'][1] . '<br>';
-echo $nams['3'][0] . '<br>';
-echo $nams['1'][0] . '<br>';
-echo $nams['2'][0] . '<br>';
-echo $nams['1'][1] . '<br>';
+echo $nums[1][1] . '<br>';
+echo $nums[2][2] . '<br>';
+echo $nums[2][1] . '<br>';
+echo $nums[2][0] . '<br>';
+echo $nums[0][0] . '<br>';
+echo $nums[1][0] . '<br>';
+echo $nums[0][1] . '<br>';
 
